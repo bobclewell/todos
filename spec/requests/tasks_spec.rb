5c62a4e9d2a5c36fc4dd2ad67050e3ff80c55eb3
Bob Clewell <bobclewell@gmail.com>
2012-05-28 16:00:59 -0400

    Basic integration tests for tasks.

--- spec/requests/tasks_spec.rb
+++ spec/requests/tasks_spec.rb
@@ -1,11 +1,47 @@
 require 'spec_helper'
 
 describe "Tasks" do
-  # describe "GET /tasks" do
-  #   it "works! (now write some real specs)" do
-  #     # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-  #     get tasks_path
-  #     response.status.should be(200)
-  #   end
-  # end
+    before(:each) do
+      @existing_user = FactoryGirl.create(:user)
+      visit root_url
+      click_link 'Log In'
+      fill_in 'email', :with => 'existing_user@email.net'
+      click_button 'Log In'
+    end
+
+    it "creates a new task with only a title" do
+      click_on "New task"
+      fill_in 'task_title', :with => 'Save the world!'
+      click_button "Save"
+      page.should have_content 'Save the world!'
+    end
+
+    it "errors if it does not have a title" do
+      click_on "New task"
+      click_button "Save"
+      page.should have_content "Title can't be blank"
+    end
+
+    it "creates a new task with title, description, and due date" do
+      click_on "New task"
+      fill_in 'task_title', :with => 'Be happy'
+      fill_in 'task_description', :with => 'Drink beer, go outside, etc.'
+      page.select("2012", :from => 'task_due_on_1i')
+      page.select("May", :from => 'task_due_on_2i')
+      page.select("13", :from => 'task_due_on_3i')
+      click_button "Save"
+      page.should have_content 'Be happy'
+      page.should have_content '2012-05-13'
+      page.should have_content 'Drink beer, go outside, etc.'
+    end
+
+    it "marks task as complete" do
+      click_on "New task"
+      fill_in 'task_title', :with => 'Save the world!'
+      click_button "Save"
+      page.should have_content 'Save the world!'
+      click_on 'completed'
+      page.should_not have_content 'Save the world!'
+    end
+
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,13 +1,7 @@
 require 'spec_helper'
 
 describe "Users" do
-  # describe "GET /users" do
-    # it "works! (now write some real specs)" do
-    #   # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-    #   get users_path
-    #   response.status.should be(200)
-    # end
-  # end
+
   it "creates a new user with an email address" do
     visit root_url
     click_link 'Sign Up'
