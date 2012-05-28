0b38e7dfd6452fa993cd7c5707a61164aeff9789
Bob Clewell <bobclewell@gmail.com>
2012-05-28 15:28:10 -0400

    Fixed redirect on initial account creation.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
-      redirect_to root_url, notice: "Thank you for signing up!"
+      session[:user_id] = @user.id
+      redirect_to root_url #, notice: "Thank you for signing up!"
     else
       render "new"
     end
