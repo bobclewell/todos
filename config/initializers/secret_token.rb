fd74faf250752e16edd69e14c40296399141409b
Bob Clewell <bobclewell@gmail.com>
2012-05-28 12:21:50 -0400

    Initial rails setup. Switched to MySQL. Testing gems installed.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Todos::Application.config.secret_token = '1ed9539cc996d9473d6655cf1d8ce9a54fa60a61a0d8c6a26f57bb45d480fc1e1959c1012757878321b8a56e0d2f0ce9895b00008da3083bccd7894575b8e53a'
